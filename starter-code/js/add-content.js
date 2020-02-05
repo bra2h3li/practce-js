97962a52c127567247921582c4b70156faf7a4b0
bra2h3li <bra2h.ee96@gmail.com>
2020-02-05 22:22:21 +0200

    update

---
+++ starter-code/js/add-content.js
@@ -0,0 +1,25 @@
+prompt("what's your Name?");
+alert("hello");
+confirm("do you like the class?");
+var name=prompt("what's your Name?");prompt("what's your name?")
+
+var name=prompt("what's your name?");
+
+
+
+
+var today = new Date();
+var hourNow =today.getHours();
+var greetings;
+if (hourNow>18){
+    greeting='Good evening!';
+}
+else if (hourNow>12){
+    greeting='Good morning!';
+
+}
+else {
+    greetinf='welcome!'
+}
+
+document.write('<h3>'+greeting+'</h3>');
